729680c1586db5c5734ccda826dd078bf0983a06
weiloong1223 <weiloong614@gmail.com>
2025-01-21 08:55:09 -0500

    final server.py

--- server.py
+++ server.py
@@ -3,19 +3,29 @@ from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
-@app.route("/emotionDetector")
+@app.route("/emotionDetector", methods=['GET'])
 def emo_detector():
-    text_to_analyse = request.args.get('textToAnalyse')
+    text_to_analyse = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyse)
-
-    emotions = response['anger']['disgust']['fear']['joy']['sadness']
+    anger_score = response['anger']
+    disgust_score = response['disgust']
+    fear_score = response['fear']
+    joy_score = response['joy']
+    sadness_score = response['sadness']
     dominant_emotion = response['dominant_emotion']
-    # Return a formatted string with the sentiment label and score
-    return "For the given statement, the system response is {}. The dominant emotion is {}."
+
+    return "For the given statement, the system response is 'anger': {anger_score}, 'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score}, and 'sadness': {sadness_score}. The dominant emotion is {dominant_emotion}.".format(
+    anger_score=anger_score,
+    disgust_score=disgust_score,
+    fear_score=fear_score,
+    joy_score=joy_score,
+    sadness_score=sadness_score,
+    dominant_emotion=dominant_emotion
+)
 
 @app.route("/")
 def render_index_page():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
