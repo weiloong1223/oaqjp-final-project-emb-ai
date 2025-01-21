2940d925c752ca7539906a11b77d6bf37cc16b06
weiloong1223 <weiloong614@gmail.com>
2025-01-21 11:53:53 +0800

    Delete emotion_detection.py

--- emotion_detection.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def emotion_detector(text_to_analyze):
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    myobj = { "raw_document": { "text": text_to_analyze } }
-    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    response = requests.post(url, json=myobj, headers=headers)
-    return response.json()['raw_document']['text']
